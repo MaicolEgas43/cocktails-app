ed6c3e6f60982b2acd4f6569897cc8b85c55db81
Maicol E Egas J <maicolegas43@gmail.com>
2024-03-26 22:15:57 -0500

    feat: implement some changes in the app, create directory view-coctails-app

---
+++ src/app/components/view-cocktails-app/view-cocktails-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-cocktails-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-cocktails-app.component.html',
+  styleUrl: './view-cocktails-app.component.scss'
+})
+export class ViewCocktailsAppComponent {
+
+}
