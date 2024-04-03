10b320305e95c554388ff069001293f69d331099
Maicol E Egas J <maicolegas43@gmail.com>
2024-04-02 23:58:07 -0500

    feat: implement material UI dependency to project

--- src/app/components/view-cocktails-app/view-cocktails-app.component.ts
+++ src/app/components/view-cocktails-app/view-cocktails-app.component.ts
@@ -1,15 +1,13 @@
-import { title } from 'process';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-view-cocktails-app',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './view-cocktails-app.component.html',
-  styleUrl: './view-cocktails-app.component.scss'
+  styleUrl: './view-cocktails-app.component.scss',
 })
 export class ViewCocktailsAppComponent {
-
-  public title: string = 'What\'s your favorite cocktail';  
-
+  public title: string = "What's your favorite cocktail";
 }
